0ef8f816952e6c19b2410ca17d14311c98c9cd39
qidirj <77445069+qidirj@users.noreply.github.com>
2026-05-29 16:28:14 +0800

    ver 0.0.20260529b 'temporary'

--- src/ui_core/ui.h
+++ src/ui_core/ui.h
@@ -17,14 +17,17 @@ using ui_event = std::function<void ()>;
 class Object {
 public:
   float x, y;
-  float w, h;
-  bool fixed; // if fixed, its (x, y) is not affected by its container
+  bool fixed; // if fixed, its (x, y) is not affected by its container (up to container)
+  unsigned w, h;
+  float opacity; // todo: implemention of opacity
+  float border; // todo: implemention of border
   sf::Color cfg = sf::Color::White, cbrd = sf::Color::White, cbg = sf::Color::Black;
   virtual void clear();
   virtual sf::RenderTexture render();
 };
 class Interactive : public Object {
 public:
+  bool on_focus;
   void clear() override;
   sf::RenderTexture render() override;
 };
--- src/ui_core/ui_base.cpp
+++ src/ui_core/ui_base.cpp
@@ -1,8 +1,40 @@
 /*
 ui > base
 
-implements: Object, Interactive, Container
+implements: Object, Interactive, Container (todo)
 */
 
 #include "ui.h"
 
+namespace ui {
+  void Object::clear() {} // default: do nothing (nothing can do)
+  sf::RenderTexture Object::render() {
+    sf::RenderTexture result({ w, h });
+    result.clear(cbg);
+
+    sf::RectangleShape brd({ float(w), float(h) });
+    brd.setOutlineColor(cbrd);
+    brd.setOutlineThickness(-2);
+    brd.setPosition({ 0, 0 });
+    result.draw(brd);
+
+    result.display();
+    return result;
+  }
+
+  void Interactive::clear() {}
+  sf::RenderTexture Interactive::render() {
+    sf::RenderTexture result({ w, h });
+    result.clear(cbg);
+
+    sf::RectangleShape brd({ float(w), float(h) });
+    brd.setOutlineColor(cbrd);
+    brd.setOutlineThickness(-2);
+    if (on_focus) brd.setOutlineThickness(-4);
+    brd.setPosition({ 0, 0 });
+    result.draw(brd);
+
+    result.display();
+    return result;
+  }
+}
